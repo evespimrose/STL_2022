9f1d267e8250b6ce92941d997cabc0022b9f903a
evespimrose <bigjht34@gmail.com>
2022-03-02 16:36:56 +0900

    프로젝트 설정 변경

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -5,13 +5,24 @@
 // - 강의 소개
 // - cppreference.com
 // - google coding convention을 참고
+// - 프로젝트 환경설정 통일
+// 
+// 할일 - 한학기 코딩한 내용을 저장할 수 있게 save 기능 완성
 //----------------------------------------------------------------------------------------
 
 #include <iostream>
+#include <string_view>
 
 using namespace std;
+
+//void save(const char*)
+void save(string_view)
+{
+
+}
 
 int main()
 {
 	cout << "C++20 STL" << endl;
+	save("소스.cpp");
 }
