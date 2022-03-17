f24026277f7279f9640e1a13bfc5225532d7ab9c
evespimrose <bigjht34@gmail.com>
2022-03-17 12:05:59 +0900

    3주 1일차_2. 사용자 지정 클래스 생성자 소멸자, unique_ptr

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -2,6 +2,9 @@
 // 2022. 1학기 STL 3월 17일 목요일
 // 수78목23 (3주 1일)
 // 
+// - 지난 시간 배운 것 - 앞으로 int[100] 대신에 -> array<int,100> 사용하자
+// - 오늘 알아본 것 - 앞으로 int* 대신에 -> unique_ptr<int> 사용하자
+//					- int*가 new int[100] 대신에 -> unique_ptr<int[]> 사용하자 
 //----------------------------------------------------------------------------------------
 
 #include <iostream>
@@ -9,37 +12,42 @@
 
 using namespace std;
 
+// 메모리 관찰용 초미니 Dog를 만들자
+
+struct Dog
+{
+	Dog() { cout << "Dog 생성자 호출" << endl; }
+	~Dog() { cout << "Dog 소멸자 호출" << endl; }
+};
+
 // [문제] 
-// 사용자 입력을 받아 원하는 만큼 int를 저장할 메모리를 확보하고 1부터 증가하는 정수로 메모리를 채워라.
-// 전체 합계를 출력하라.
+// 
+// 
+
+void f()
+{
+	throw 1;
+}
 
 int main()
 {
 	save("소스.cpp");
 
-	while(true) 
+	/*for (int i = 0; i < 3; ++i)
 	{
-		int num;
-		cout << "몇 개를 원하십니까? : ";
-		cin >> num;
+		Dog* p = new Dog;
+		delete p;
+	}*/
+	unique_ptr<Dog[]>p{new Dog[3]};
 
-		unique_ptr<int[]> p{new int[num]};
-
-		for (int i = 0; i < num; ++i)
-		{
-			p[i] = i + 1;
-		}
-
-		long long sum{};
-
-		for (int i = 0; i < num; ++i)
-		{
-			sum += p[i];
-		}
-
-		cout << "1 부터" << num << "까지의 합계 : " << sum << endl << endl;
-
-		p.reset(nullptr);
+	// 여기서 비정상 종료
+	try
+	{
+		f();		// 예외를 던진다
+	}
+	catch (...)		// ... elipses
+	{
+		cout << "예외를 잡았다" << endl;
+		return 0;
 	}
 }
-
