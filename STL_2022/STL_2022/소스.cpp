2692f51a410ed56c8b85af8fd23dd3f78bb7e458
evespimrose <bigjht34@gmail.com>
2022-03-24 11:08:53 +0900

    4주 1일차_2. 람다함수 호출가능타입

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -16,16 +16,47 @@
 using namespace std;
 
 // [문제] 
+// 호출가능(Callable) 타입
+// 호출가능한 타입은 어떤 것들이 있나?
+// 1. 함수 - CODE 세그먼트에 기록된 명령어 집합의 시작번지
+// 2. 람다 - 이름 없는 함수
+//
 
+class Cat {};
 
+auto x2 = []() {
+	cout << "안녕! 난 람다야! 하위!" << endl;
+};
 
 int main()
 {
-	cout << "메인 호출" << endl;
+	class Dog {};
 
-	int(*t)(void) = (int(*)(void))main();
-	
-	t();
+
+
+
+	//lambda
+	auto x = []() {
+		cout << "안녕! 난 람다야! 하위!" << endl;
+	};
+
+	auto x1 = []() {
+		cout << "안녕! 난 람다야! 하위!" << endl;
+	};
+
+	cout << typeid(Dog).name() << endl;
+	// class `int __cdecl main(void)'::`2'::Dog
+	cout << typeid(Cat).name() << endl;
+	// class Cat
+	cout << typeid(x).name() << endl;
+	//class `int __cdecl main(void)'::`2'::<lambda_1>
+	cout << typeid(x1).name() << endl;
+	//class `int __cdecl main(void)'::`2'::<lambda_2>
+	cout << typeid(x2).name() << endl;
+	//class x2::<lambda>
+
+	// 람다함수는 클래스로 구현되어 있음.
+
 
 	//save("소스.cpp");
 }
