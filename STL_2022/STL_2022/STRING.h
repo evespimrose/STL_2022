df3a1d69ae5502655f6d6f725b52cf507c7bc22f
evespimrose <bigjht34@gmail.com>
2022-03-24 12:19:39 +0900

    4주 1일차_6. 오류 수정

--- STL_2022/STL_2022/STRING.h
+++ STL_2022/STL_2022/STRING.h
@@ -8,11 +8,19 @@ public:
 		id = ++cid;
 
 		// 관찰메시지를 켜면 출력한다.
-		cout << "ctor [" << id << "] " << this << endl;
+		std::cout << "ctor [" << id << "] " << this << std::endl;
+	};
+	STRING(int n, const char* s) : num{ n }, p{ new char[num] } {
+		memcpy(p, s, num);
+
+		id = ++cid;
+
+		// 관찰메시지를 켜면 출력한다.
+		std::cout << "ctor [" << id << "] " << this << std::endl;
 	};
 	~STRING() {
 		// 관찰메시지를 켜면 출력한다.
-		cout << "dtor [" << id << "] " << this << endl;
+		std::cout << "dtor [" << id << "] " << this << std::endl;
 
 		delete[] p;
 	}
@@ -24,6 +32,8 @@ private:
 	static int cid;						// 객체가 생성될 때마다 1 증가
 
 	friend std::ostream& operator<<(std::ostream&, const STRING&);
+	friend STRING operator+(STRING&, STRING&);
+
 };
 
 int STRING::cid{};
@@ -33,4 +43,13 @@ std::ostream& operator<<(std::ostream& os, const STRING& s)
 	for (int i = 0; i < s.num; ++i)
 		os << s.p[i];
 	return os;
+}
+
+STRING operator+(STRING& l, STRING& r)
+{
+	char* ch = new char[l.num + r.num];
+
+	//STRING tmp{ l.num + r.num , l.p + r.p};
+
+
 }
