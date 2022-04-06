2b60f8cd50284a8b006e507f95b4f12d55ad807e
evespimrose <bigjht34@gmail.com>
2022-04-06 16:09:38 +0900

    5주 2일차_1. 생성 위치

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -1,24 +1,24 @@
 //----------------------------------------------------------------------------------------
-// 2022. 1학기 STL 3월 31일 목요일
-// 수78목23 (5주 1일)
+// 2022. 1학기 STL 4월 06일 요일
+// 수78목23 (5주 2일)
 //
 // RVO(Return value optimization), Copy Ellision
 // 
 // 컨테이너 -> Containers are objects that store other objects
 // 
 // 컨테이너
-// Sequence container
+// Sequence container		- 임의의 위치에 원소의 값을 읽고 쓰거나 추가할 수 있다.
 //		array				- 유일하게 원소 갯수가 고정됨.
 //		vector				- 
 //		deque
 //		foward_list
 //		list
 // 
+// 예정 - 중간시험 4.27(수) (8주 2일)
+// 
 //----------------------------------------------------------------------------------------
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
 
 #include "save.h"
 #include "STRING.h"
@@ -27,24 +27,14 @@ using namespace std;
 
 extern bool 관찰;
 
-// [문제] 키보드에서 입력하는 단어를 모두 읽어라. 입력이 끝나면 오름차순으로 정렬해서 출력해라.
+// [문제] 
 
 int main()
 {
-	vector<string> v;
-
-	string s;
-
-	while (cin >> s)
-	{
-		v.push_back(s);
-	}
-
-	// 내림차순 정렬한다.  (lexicorgraphical comparison)
-	sort(v.begin(), v.end(), [](const string& a, const string& b) {return a > b; });
-
-	for (string s : v)
-		cout << s << '\t';
+	관찰 = true;
+	
+	unique_ptr<STRING> p{ new STRING { "12345" } };
+	STRING s{ "12345" };		// 생성위치가 다르다.
 
 	//save("소스.cpp");
 }
