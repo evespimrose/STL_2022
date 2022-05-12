94a2fb09c6c377ef3022e086ee193dfeff28e183
evespimrose <bigjht34@gmail.com>
2022-05-12 12:00:24 +0900

    11주 1일차_5. 여러가지 정렬방법

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -29,6 +29,7 @@ extern bool 관찰;
 
 // [문제]
 // 이 소설에서 각 소문자의 빈도를 세라.
+// 가장 많이 사용된 단어 순으로 출력하라.
 //
 
 int main()
@@ -55,13 +56,40 @@ int main()
 		cout << "[" << 소문자 << "] - " << 갯수 << endl;
 	*/
 
-	map<char, int, greater<int>> mcig{};
+	/*map<char, int, greater<int>> mcig{};
 
 	for (auto [소문자, 갯수] : mc)
 		mcig[소문자] = 갯수;
 
 	for (auto [소문자, 갯수] : mcig)
-		cout << "[" << 소문자 << "] - " << 갯수 << endl;
+		cout << "[" << 소문자 << "] - " << 갯수 << endl;*/
+
+	map<string, int> si{};
+
+	for (const string& s : v)
+		si[s]++;
+
+	multimap<int, string, greater<int>> sis{};
+	for (auto [단어, 갯수] : si)
+		sis.emplace(make_pair(갯수, 단어));
+
+	//// 앞에서부터 20개만
+
+	//auto p = sis.begin();
+	//for (int i = 0; i < 20; ++i, ++p)
+	//	cout << p->second << " - " << p->first << endl;
+
+	// 뒤에서부터 20개만
+
+	auto p = sis.rbegin();
+	for (int i = 0; i < 20; ++i, ++p)
+		cout << p->second << " - " << p->first << endl;
+
+	// 단어 갯수가 75041개인지 검증한다.
+	int cnt{};
+	for (auto [갯수, 단어] : sis)
+		cnt += 갯수;
+	cout << cnt << endl;
 
 	//save("소스.cpp");
 }
