a15e7a0e8884309150a1ebd4cab026d611fef98c
evespimrose <bigjht34@gmail.com>
2022-05-12 10:39:44 +0900

    11주 1일차_1. 연관 배열 기초

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -1,18 +1,22 @@
 //----------------------------------------------------------------------------------------
-// 2022. 1학기 STL 5월 11일 수요일
-// 수78목23 (10주 2일)
+// 2022. 1학기 STL 5월 12일 목요일
+// 수78목23 (11주 1일)
 // 
 // 5월 25일 졸작 설명회(수 56교시)
 // 5/30 ~ 6/20 지도교수 신청일
 // 
+// Sequential Cntainer - array, vector, deque, fowardlist, list
 // Associative Container - set, multi_set, map, multi_map
-// set - 언제나 key를 compare로 정렬하고 있다. 찾기/추가/삭제 O(log N)
-//      - set의 주요 동작을 알아본다. (빨리 찾기 위한 컨테이너)
-//      - 내가 만든 자료형(class Dog)을 set에 넣어 관리
+//	map<key, value, key_compare>
+//		- 연관배열(associative array)로 사용할 수 있다.
+// PHP?
+// 
+// unordered Associative Container - u_set, u_multiset, u_map, u_multimap
 // 
 //----------------------------------------------------------------------------------------
 #include <iostream>
-#include <set>
+#include <map>
+#include <fstream>
 
 #include "save.h"
 #include "STRING.h"
@@ -22,27 +26,20 @@ using namespace std;
 extern bool 관찰;
 
 // [문제]
-// 키보드 입력한 단어를 set<STRING>에 넣어라
-// set<STRING>의 정렬기준은 단어길이 오름차순이다
-// set을 화면에 출력하라
+// 소설에 사용된 알파벳의 빈도수를 출력하라.
 //
-
-class 길이순
-{
-public:
-	bool operator()(const STRING& a, const STRING& b) const
-	{
-		return a.getNum() < b.getNum();
-	}
-};
 
 int main()
 {
+	// 이름과 전화번호 
+	map<string, string> m;
+
+	m.insert(pair("홍길동", "01012345678"));
+
+	m["마우스"] = "12345678";				// 연관배열로 사용하는 것이 이해하기 편함. 집어 넣는데에 사용되기도 하고 읽을때 사용하기도 함.
+
+	for (auto [이름, 번호] : m)
+		cout << 이름 << " - " << 번호 << endl;
+
 	save("소스.cpp");
-
-	set<STRING, 길이순> s{ istream_iterator<STRING>{cin}, {} };
-	
-	for (const STRING& word : s)
-		cout << word << endl;
-
 }
