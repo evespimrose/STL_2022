ce7e20748c888af757b9756acb991f6cfb33d441
evespimrose <bigjht34@gmail.com>
2022-05-25 16:10:57 +0900

    12주 2일차_4. unique 알고리즘

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -14,8 +14,6 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <numeric>
-#include <random>
 
 #include "save.h"
 #include "STRING.h"
@@ -24,28 +22,19 @@ using namespace std;
 
 extern bool 관찰;
 
-random_device rd;
-default_random_engine dre{ rd() };
-
 // [문제]
-// 랜덤한 수 n개를 화면에 출력
+// 
 //
 
 int main()
 {
-	//vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	vector<int> v{ 1, 2, 3, 3, 3, 5, 3 };
 
-	// sample : 로또 번호뽑는 알고리즘
+	v.erase(unique(v.begin(), v.end()),v.end());
 
-	vector<int> v(45);		// int{}를 45번 호출
-
-	iota(v.begin(), v.end(), 1);
-
-	//for (int i : v)
-	//	cout << i << " ";
-	//cout << endl;
-
-	sample(v.begin(), v.end(), ostream_iterator<int>{cout, " "}, 6, dre);
+	for (int i : v)
+		cout << i << " ";
+	cout << endl;
 
 	//save("소스.cpp");
 }
