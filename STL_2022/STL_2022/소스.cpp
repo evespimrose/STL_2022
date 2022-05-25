04ad38fe0a0f775773d7a6ad6454d957e559d771
evespimrose <bigjht34@gmail.com>
2022-05-25 15:42:55 +0900

    12주 2일차_1. mutating 알고리즘 - copy

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------------------
-// 2022. 1학기 STL 5월 19일 목요일
+// 2022. 1학기 STL 5월 25일 수요일
 // 수78목23 (12주 1일)
 // 
 // 5월 25일 졸작 설명회(수 56교시)
@@ -8,14 +8,12 @@
 // 알고리즘
 //  - non-modifying sequence operation(원소를 수정하지 않는 알고리즘)
 // 
+// 시험 - 15주 1일 : 6월 15일 수요일
 // 
 //----------------------------------------------------------------------------------------
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <string>
-#include <thread>
-#include <format>
+#include <vector>
 
 #include "save.h"
 #include "STRING.h"
@@ -25,23 +23,19 @@ using namespace std;
 extern bool 관찰;
 
 // [문제]
-// 전광판처럼 흘러가게 찍어보자.
+// copy는 왜 mutating 알고리즘인가?
 //
 
 int main()
 {
+	vector v{ 1,2,3,4,5 };
+	// 목적지가 자기 자신이 될 수 있기 때문에 mutating 알고리즘이다.
+
+	copy(v.begin(), v.end(), inserter(v, v.begin() + 1));
+
+	for (int i : v)
+		cout << i << " ";
+	cout << endl;
+
 	//save("소스.cpp");
-
-	string str{ "[South Korea and Japan just don't get along. ]" };
-
-	for (int i = 0; i < 12; ++i)
-		cout << endl;
-
-	while (true)
-	{
-		rotate(str.begin() + 1, str.begin() + 2, str.end() - 1);
-		cout << format("{:^70}",str) << "\r";
-		this_thread::sleep_for(100ms);
-	}
-
 }
