9eb0d54964d8265dc854827fdc00c5819a707b16
evespimrose <bigjht34@gmail.com>
2022-05-26 11:24:39 +0900

    13주 1일차_3. type_traits 연습

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -17,6 +17,9 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <iterator>
+#include <compare>
+#include <list>
 
 #include "save.h"
 #include "STRING.h"
@@ -25,29 +28,40 @@ using namespace std;
 
 extern bool 관찰;
 
-template<class Iter>            // generic한 코드.
-//vector<int>::difference_type dist(vector<int>::iterator b, vector<int>::iterator e)
-long long dist(Iter b, Iter e)
+template<class T>
+long long dist(T b, T e)
 {
     // 이 코드는 반복자에서만 실행되었으면 좋겠다.
-    return e - b;
+    // T는 정말로 반복자인가?
+    // 템플릿 코드에서는 전달되는 자료형 T가 원하는 조건을 만족하는지 검사할 필요가 있다.
+    // -> type traits를 사용하면 된다.
+    // 
+    // 랜덤반복자만 실행되게 할 수 있나?
+    // 
+    //if (is_same_v<typename iterator_traits<T>::iterator_category, random_access_iterator_tag>)       // type_traits_ 랜덤반복자 태그
+    //if (is_same_v<typename iterator_traits<T>::iterator_category, input_iterator_tag>)       // type_traits_ 입력반복자 태그
+    if (is_base_of_v< input_iterator_tag, typename iterator_traits<T>::iterator_category>)       // type_traits_ 입력반복자 태그
+        cout << "반복자다" << endl;
+    else
+        cout << "반복자가 아니다" << endl;
+    return 1;
 }
-
-template<>
-long long dist<int>(int b, int e)
-{
-    cout << "int일 때 - ";
-    return e - b;
-}
-
-
 
 int main()
 {
     vector<int> v{ 1,2,3,4,5 };
 
-    cout << "반복자 간의 거리 - " << dist(v.begin(), v.end()) << endl;
-    cout << dist(1, 100) << endl;
+    list<int> ist{ 1,2,3 };
 
-    save("소스.cpp");
+    int arr[10];
+
+    STRING s;
+
+    dist(v.begin(), v.end());
+    dist(ist.begin(), ist.end());
+    dist(arr, arr + 10);
+    dist(s.begin(), s.end());
+    
+
+    //save("소스.cpp");
 }
