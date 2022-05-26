1099e2f5f506a8f9ce9860974b4e844830057671
evespimrose <bigjht34@gmail.com>
2022-05-26 11:50:53 +0900

    13주 1일차_4. constexpr-if 문법

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -31,37 +31,32 @@ extern bool 관찰;
 template<class T>
 long long dist(T b, T e)
 {
-    // 이 코드는 반복자에서만 실행되었으면 좋겠다.
-    // T는 정말로 반복자인가?
-    // 템플릿 코드에서는 전달되는 자료형 T가 원하는 조건을 만족하는지 검사할 필요가 있다.
-    // -> type traits를 사용하면 된다.
+    // 만일 T가 랜덤반복자라면
+    //         e-b가 거리이다.
+    // 그렇지 않다면
+    //         b부터 몇 번 이동했는지 세야한다.
     // 
-    // 랜덤반복자만 실행되게 할 수 있나?
-    // 
-    //if (is_same_v<typename iterator_traits<T>::iterator_category, random_access_iterator_tag>)       // type_traits_ 랜덤반복자 태그
-    //if (is_same_v<typename iterator_traits<T>::iterator_category, input_iterator_tag>)       // type_traits_ 입력반복자 태그
-    if (is_base_of_v< input_iterator_tag, typename iterator_traits<T>::iterator_category>)       // type_traits_ 입력반복자 태그
-        cout << "반복자다" << endl;
-    else
-        cout << "반복자가 아니다" << endl;
-    return 1;
+    if (is_base_of_v< input_iterator_tag, typename iterator_traits<T>::iterator_category>)
+    {
+        if constexpr (is_same_v<random_access_iterator_tag, typename iterator_traits<T>::iterator_category>)       // type_traits_ 랜덤반복자 태그
+   //if (is_same_v<typename iterator_traits<T>::iterator_category, input_iterator_tag>)       // type_traits_ 입력반복자 태그
+   //if (is_base_of_v< input_iterator_tag, typename iterator_traits<T>::iterator_category>)       // type_traits_ 입력반복자 태그
+            return e - b;
+        // 랜덤반복자가 아닌 경우에더 e-b 문장이 생성되어 컴파일 오류 발생
+        // - 이전 해결 방법 : tag dispatching
+        // c++17부터는 constexpr if로 선택적 컴파일 가능
+        else
+            return 100;
+    }
 }
 
 int main()
 {
     vector<int> v{ 1,2,3,4,5 };
-
     list<int> ist{ 1,2,3 };
-
-    int arr[10];
-
-    STRING s;
 
     dist(v.begin(), v.end());
     dist(ist.begin(), ist.end());
-    dist(arr, arr + 10);
-    dist(s.begin(), s.end());
-    
 
     //save("소스.cpp");
 }
