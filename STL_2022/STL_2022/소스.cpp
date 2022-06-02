5333951bee577f4078bb84cf6bc4e32acfc35949
evespimrose <bigjht34@gmail.com>
2022-06-02 11:32:27 +0900

    13주 2일차_3. ranges의 projection들

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -16,8 +16,9 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <list>
+#include <string>
 #include <ranges>
+#include <random>
 
 #include "save.h"
 #include "STRING.h"
@@ -27,7 +28,7 @@ using namespace std;
 extern bool 관찰;
 
 // [문제]
-// v를 내림차순 정렬하고 출력하라.
+// v를 이름 오름차순으로 정렬하고 출력하라.
 //
 
 // ranges는 알고리즘과 iterator를 확장시키고 일반화시킨 것
@@ -36,16 +37,41 @@ extern bool 관찰;
 // 
 //
 
+default_random_engine dre;
+uniform_int_distribution<> uidNum{ 1,1000 };
+uniform_int_distribution<> uidName{ 'a', 'z' };
+
+class Dog
+{
+    string name;
+    int num;
+    
+public:
+    Dog()
+    {
+        for (int i = 0; i < 10; ++i)
+            name += uidName(dre);
+        num = uidNum(dre);
+    }
+
+    string getName() const { return name; }
+
+    int getNum() const { return num; }
+
+    void show() const { cout << name << " - " << num << endl; }
+};
+
 int main()
 {
-    vector<int> v{ 1,2,3,4,5,6,7,8,9,10 };
+    vector<Dog> v(100);
 
-    //ranges::sort(v);
-    ranges::sort(v, greater<int>{});
+    //ranges::sort(v, {}, &Dog::getName);
+    ranges::sort(v, less<int>(), &Dog::getNum);
+    //ranges::sort(v, less<string>(), &Dog::getName);           // name 기준 오름차순
+    //ranges::sort(v, greater<string>(), &Dog::getName);        // name 기준 내림차순
 
-    for (int i : v)
-        cout << i << " ";
-    cout << endl;
+    for (const Dog& dog : v)
+        dog.show();
 
     //save("소스.cpp");
 }
