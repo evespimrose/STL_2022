1ac1bb81a3441a6e3beebc2e47faf3b66807495c
evespimrose <bigjht34@gmail.com>
2022-06-08 16:05:18 +0900

    14주 1일차_views

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -1,6 +1,6 @@
 //----------------------------------------------------------------------------------------
-// 2022. 1학기 STL 6월 2일 목요일
-// 수78목23 (13주 2일)
+// 2022. 1학기 STL 6월 8일 수요일
+// 수78목23 (14주 1일)
 // 
 // 5/30 ~ 6/20 지도교수 신청일
 // 
@@ -14,37 +14,39 @@
 // 
 //----------------------------------------------------------------------------------------
 #include <iostream>
-#include <algorithm>
 #include <ranges>
+#include <format>
 
 #include "save.h"
 #include "STRING.h"
+
+// constrained algorithm : concept으로 coding
+// span<T> : [], string, array, vector
+// [문제] 소수(prime number)를 출력하라.
+//
 
 using namespace std;
 
 extern bool 관찰;
 
-// [문제]
-// 
-//
+bool isPrime(int n)
+{
+    for (int i = 2; i <= n / 2; ++i)
+        if (n % i == 0)
+            return false;
+    return true;
+}
 
 int main()
 {
-    int a[]{ 1,2,3,4,5,6,7,8,9,10 };
+    // [문제] 소수(prime number)를 100개만 출력하라.
+    for (int n : views::iota(2) | views::filter(isPrime)|views::take(100)
 
-    auto v = views::all(a);
+        )
+    {
+        cout << format("{:8}", n);
+    }
 
-    cout << "a의 크기 " << sizeof(a) << endl;
-
-    cout << "a의 모든 원소로 구성된 view의 크기" << sizeof(v) << endl;
-
-    cout << "v의 타입 : " << typeid(v).name() << endl;
-
-    *v.begin() = 200;
-
-    for (int n : a)
-        cout << n << " ";
-    cout << endl;
 
     //save("소스.cpp");
 }
