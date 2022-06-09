d8ff3205ac0bf12769b0be82e0bf4c7ad51c47ec
evespimrose <bigjht34@gmail.com>
2022-06-09 12:17:58 +0900

    14주 2일차_3. 애너그램 많은순으로 정렬후 출력 - list 써서

--- STL_2022/STL_2022/소스.cpp
+++ STL_2022/STL_2022/소스.cpp
@@ -1,41 +1,37 @@
-//----------------------------------------------------------------------------------------
-// 2022. 1학기 STL 6월 9일 목요일
-// 수78목23 (14주 2일)
+//-----------------------------------------------------------------------------
+// 2022. 1학기 STL
 // 
-// 5/30 ~ 6/20 지도교수 신청일
-// 
-// C++20 언어 4가지 핵심 변화
-// concepts
-// range / view
-// coroutine
-// module
-// 
-// 시험 - 15주 1일 : 6월 15일 수요일
-// 
-//----------------------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+
 #include <iostream>
 #include <fstream>
-#include <algorithm>
-#include <vector>
 #include <string>
+#include <vector>
+#include <algorithm>
 #include <ranges>
+#include <list>
 
 #include "save.h"
 #include "STRING.h"
-
-// [문제] 벡터가 단어쌍을 저장하도록 한다.
-// first는 정렬하고 second는 원래 단어를 저장한다.
-// v에 저장된 WP를 출력해본다.
-// 단어를 입력받아 anagram을 출력하라.
-//
 
 using namespace std;
 
 extern bool 관찰;
 
+bool isPrime(int n)
+{
+    for (int i = 2; i <= n / 2; ++i)
+        if ((n % i) == 0)
+            return false;
+    return true;
+}
+
+// 벡터가 단어쌍을 저장하도록 한다
+// first는 정렬하고 second는 원래 단어를 저장한다
+
 struct WP : public pair<string, string>
 {
-    WP(const string& s) : pair<string, string>{ s,s } 
+    WP(const string& s) : pair < string, string>{ s, s }
     {
         sort(first.begin(), first.end());
     }
@@ -43,24 +39,65 @@ struct WP : public pair<string, string>
 
 int main()
 {
+    save("소스.cpp");
+
+    // [문제] "단어들.txt"에 있는 단어를 읽고 갯수를 출력하라
+
     ifstream in{ "단어들.txt" };
-    ofstream out{ "애너그램들.txt" };
-    //save("소스.cpp");
-    
-    vector<WP> v{ istream_iterator<string>{in},{} };
+
+    vector<WP> v{ istream_iterator<string>{ in },{} };
+    cout << "단어의 개수 - " << v.size() << endl;
+
+    // v를 first 기준 오름차순으로 정렬한다.
+    //sort(v.begin(), v.end(), [](const WP& w, const WP& p) {return w.first < p.first;});
     ranges::sort(v, {}, &WP::first);
 
+    // [문제] 모든 anagram을 저장하라
+    ofstream out{ "애너그램들.txt" };
+
     auto b = v.begin();
+
     int cnt{ 1 };
+
+    vector<list<string>> vv;
+    vv.reserve(31000);
+
     while (true)
     {
-        auto i = adjacent_find(b, v.end(), [](const WP& a, const WP& b) {return a.first == b.first; });
-        auto j = find_if_not(i + 1, v.end(), [&i](const WP& a) {return a.first == i->first; });
-        // [i,j)는 애너그램 구간이다.
-        out << "[" << cnt << "] - ";
+        auto i = adjacent_find(b, v.end(), [](const WP& a, const WP& b)
+            {
+                return a.first == b.first;
+            });
+
+        if (i == v.end())
+            break;
+
+        auto j = find_if_not(i + 1, v.end(), [i](const WP& a)
+            {
+                return a.first == i->first;
+            });
+
+        // [i, j) 는 애너그램 구간이다
+        list<string> cont;
         for (auto p = i; p < j; ++p)
-            out << p->second << " ";
-        out << endl;
-        ++cnt;
+            cont.push_back(p->second);
+        vv.push_back(cont);
+        b = j;
+    }
+
+    // vv를 list의 갯수 순으로 내림차순 정렬
+    sort(vv.begin(), vv.end(), [](const list<string>& a, const list<string>& b)
+        {
+            return a.size() > b.size();
+        });
+
+    cout << "애너그램 많은 수 - " << vv.begin()->size() << endl;
+    for (const list<string>& cont : vv | views::take(10))
+    {
+        for (const string& s : cont)
+        {
+            cout << s << " ";
+        }
+        cout << endl;
     }
 }
